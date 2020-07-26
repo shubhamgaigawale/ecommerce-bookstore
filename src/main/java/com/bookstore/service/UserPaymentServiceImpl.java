0649b10b671da186e5b1a53cf04216a8255409ce
Shubham Gaigawale <shubhamgawale1999@outlook.com>
2020-07-26 23:20:32 +0530

    code commit

---
+++ src/main/java/com/bookstore/service/UserPaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.UserPayment;
+import com.bookstore.repository.UserPaymentRepository;
+
+@Service
+public class UserPaymentServiceImpl {
+	@Autowired
+	private UserPaymentRepository userPaymentRepository;
+	
+	public UserPayment findById(Long id) {
+		return userPaymentRepository.findOne(id);
+	}
+	
+	public void removeById(Long id) {
+		userPaymentRepository.delete(id);
+	}
+	
+}
